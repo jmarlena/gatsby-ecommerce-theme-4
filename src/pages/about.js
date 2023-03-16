66a5d4dae46ec7149e67bcada6dbfde65a3c15e4
jmarlena <6732600+jmarlena@users.noreply.github.com>
2023-03-16 16:41:32 -0700

    Update about.js

--- src/pages/about.js
+++ src/pages/about.js
@@ -27,7 +27,7 @@ const AboutPage = (props) => {
         <Hero
           maxWidth={'900px'}
           image={'/about.png'}
-          title={`Sydney \n A British brand since 1860`}
+          title={`Sydney \n A British brand since 1609`}
         />
 
         <div className={styles.navContainer}>
