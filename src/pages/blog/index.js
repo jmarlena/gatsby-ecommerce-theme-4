ebba87fd6c1cfb388966c6d88bbd6e2552bfc938
jmarlena <6732600+jmarlena@users.noreply.github.com>
2023-12-11 13:18:50 -0800

    Update index.js

--- src/pages/blog/index.js
+++ src/pages/blog/index.js
@@ -19,7 +19,7 @@ const BlogPage = (props) => {
         <Hero
           maxWidth={'400px'}
           image={'/blogCover.png'}
-          title={`The new standard of Closing`}
+          title={`The new standard of Sipping tea`}
           ctaLink={'read story'}
           ctaTo={'/blog/sample'}
           header={'design'}
